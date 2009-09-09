ec4660bb1ad88151002e24165bcc54e396853c2a
inshadow <inshadow@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-09 11:18:16 +0000

    Fixed bug #4490
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/roles2/trunk@24187 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.2.2',
+	'version' => '0.2.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.1.0-0.0.0',
+			'typo3' => '4.2.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
